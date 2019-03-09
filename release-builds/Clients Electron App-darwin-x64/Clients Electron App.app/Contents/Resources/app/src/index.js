f9c81dcbbbc772ebdcb512f158157afc716f68f3
mariomuratori <mariomuratori@gmail.com>
2019-03-09 09:32:28 -0300

    message

---
+++ release-builds/Clients Electron App-darwin-x64/Clients Electron App.app/Contents/Resources/app/src/index.js
@@ -0,0 +1,106 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+
+const url = require('url');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') {
+require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+    })
+}
+
+let mainWindow
+let newProductWindow
+
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/index.html'),
+        protocol: 'file',
+        slashes: true,
+    }))
+
+    const mainMenu = Menu.buildFromTemplate(templateMenu)
+    Menu.setApplicationMenu(mainMenu);
+
+    mainWindow.on('closed', () => {
+        app.quit();
+    });
+});
+
+function createNewProductWindow() {
+    newProductWindow = new BrowserWindow({
+        width: 400,
+        height: 330,
+        title: 'Agregar un nuevo cliente'
+    });
+    //newProductWindow.setMenu(null);
+    newProductWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/new-product.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+    newProductWindow.on('closed', () => {
+        newProductWindow = null;
+    });
+}
+
+ipcMain.on('product:new', (e, newProduct) => {
+    mainWindow.webContents.send('product:new', newProduct);
+    newProductWindow.close();
+});
+
+const templateMenu = [
+    {
+        label: 'Archivo',
+        submenu: [
+            {
+                label: 'Nuevo cliente',
+                accelerator: process.platform == 'darwin' ? 'command+N' : 'Ctrl+N',
+                click() {
+                    createNewProductWindow();
+                }
+            },
+    {
+        label: 'Borrar todos los clientes',
+        accelerator: process.platform == 'darwin' ? 'command+X' : 'Ctrl+X',
+       click() {
+            mainWindow.webContents.send('product:remove-all');
+        }
+    },
+    {
+        label: 'Salir',
+        accelerator: process.platform == 'darwin' ? 'command+Q' : 'Ctrl+Q',
+        click() {
+            app.quit();
+        }
+    }
+]
+    }
+];
+
+if(process.platform == 'darwin') {
+    templateMenu.unshift({
+        label: app.getName()
+    });
+};
+
+if(process.env.NODE_ENV !== 'production') {
+    templateMenu.push({
+        label: 'Herramientas',
+        submenu: [
+            {
+            label: 'Mostrar/Ocultar herramientas',
+            accelerator: process.platform == 'darwin' ? 'Comand+D' : 'Ctrl+D',
+            click(item, focusedWindow) {
+                focusedWindow.toggleDevTools();
+            }
+        },
+        {
+            label: 'Refrescar',
+            role: 'reload'
+        }
+    ]
+    })
+}
